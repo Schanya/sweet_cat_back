f18653164cb5fc58f056d472382d7d4f070dc0fd
DESKTOP-U2N9G19\37529 <anyashemelevadm@gmail.com>
2021-10-10 11:15:49 +0300

    feat: delete entities to module

--- src/entities/entities.module.ts
+++ src/entities/entities.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Cat } from './cats/cats.entity';
-import { Breed } from './cats_breeds/cats_breeds.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cat, Breed])],
+  imports: [TypeOrmModule.forFeature([])],
   controllers: [],
   providers: [],
 })
