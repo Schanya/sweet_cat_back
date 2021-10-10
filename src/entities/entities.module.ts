df45f1ee9cf84e8d39a049075e107737373fa826
DESKTOP-U2N9G19\37529 <anyashemelevadm@gmail.com>
2021-10-10 11:30:09 +0300

    feat: changed .entities.module.ts

--- src/entities/entities.module.ts
+++ src/entities/entities.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [],
   controllers: [],
   providers: [],
 })
